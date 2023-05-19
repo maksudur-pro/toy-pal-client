6c4262fecedfc35407a8d9345e2d3b0f159da71b
unknown <ajoy9532@gmail.com>
2023-05-19 14:45:07 +0600

    added env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMVhxIBmLnTg-BzxsoJtfDIXBV3w8Q6uw",
-  authDomain: "toy-pal.firebaseapp.com",
-  projectId: "toy-pal",
-  storageBucket: "toy-pal.appspot.com",
-  messagingSenderId: "388178801972",
-  appId: "1:388178801972:web:28a2c5365e1d7e4fcaa161",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
